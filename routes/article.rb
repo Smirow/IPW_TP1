395c9b9886b6ee2bdf479c381b4288c4a0579d4c
Smirow <magued1509@gmail.com>
2017-02-24 15:01:35 +0100

    Reverse articles in index

--- routes/article.rb
+++ routes/article.rb
@@ -1,9 +1,9 @@
 get '/' do
     articles = []
     @content = ""
-    8.times do |i|
-        articles[i] = Article.new("articles/#{i+1}.txt")
-        @content += "<div class=\"article-title\"><a href=\"articles/#{i+1}\" target=\"_blank\"> #{articles[i].title} </a></div>"
+    8.downto(1) do |i|
+        articles[i] = Article.new("articles/#{i}.txt")
+        @content += "<div class=\"article-title\"><a href=\"articles/#{i}\" target=\"_blank\"> #{articles[i].title} </a></div>"
         @content += "<div class=\"article-content\"> #{articles[i].content[0...400]}... </div>"
     end
     erb :index
