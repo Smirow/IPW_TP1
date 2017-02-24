d4b940af418e55ad537b594be4d23e42f4670d10
Smirow <magued1509@gmail.com>
2017-02-24 15:14:32 +0100

    Add article div

--- routes/article.rb
+++ routes/article.rb
@@ -3,8 +3,10 @@ get '/' do
     @content = ""
     8.downto(1) do |i|
         articles[i] = Article.new("articles/#{i}.txt")
+        @content += "<div class=\"article\">"
         @content += "<div class=\"article-title\"><a href=\"articles/#{i}\" target=\"_blank\"> #{articles[i].title} </a></div>"
         @content += "<div class=\"article-content\"> #{articles[i].content[0...400]}... </div>"
+        @content += "</div>"
     end
     erb :index
 end
